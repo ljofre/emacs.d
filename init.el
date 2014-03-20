c5fee3c149040374dd34a2347a0e65367908ee9e
leonardo jofre <ljofre2146@gmail.com>
2014-03-19 23:34:26 -0300

    ultimos cambios mac

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(elpy-rpc-python-command "//anaconda/bin/python")
- '(python-shell-interpreter "//anaconda/bin/python"))
+ '(python-shell-interpreter "~/anaconda/bin/python"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
